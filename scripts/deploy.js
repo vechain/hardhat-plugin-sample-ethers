d68fdc84937f74fd742d4046c0d06ec3ab3e27e0
Mario Micklisch <mario@favo.org>
2023-04-12 13:02:24 +0200

    Use correct storage example contract in deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Storage = await hre.ethers.getContractFactory("Storage");
+  const storage = await Storage.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await storage.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Storage deployed to ${storage.address}`
   );
 }
 
