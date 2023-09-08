a1f84d56e9794591d32c1b4f27658cfe9c28cda1
Makis Christou <michalis.christou@protonmail.com>
2023-09-08 12:12:48 +0300

    Upgrade sample project to ethers v6

--- test/Storage.test.ts
+++ test/Storage.test.ts
@@ -1,6 +1,8 @@
-import {ethers} from "hardhat";
-import {ClausesBuilder} from "../../hardhat-plugins/packages/vechain/dist/clausesBuilder";
-
+import { ethers } from "hardhat";
+import { HttpNetworkConfig } from "hardhat/types";
+import { ClausesBuilder } from "../../hardhat-plugins/packages/vechain/dist/clausesBuilder";
+import { createWallet } from "../../hardhat-plugins/packages/vechain/dist/helpers/createWallet";
+import { createProvider} from "../../hardhat-plugins/packages/vechain/dist/helpers/createProvider";
 const { expect } = require("chai");
 
 describe("Storage", function () {
@@ -14,17 +16,33 @@ describe("Storage", function () {
 
     const Storage = await ethers.getContractFactory("Storage");
     const storage = await Storage.deploy();
-    const abi = Storage.interface.format(ethers.utils.FormatTypes.json) as string;
-    await storage.deployed();
+    const abiArray = Storage.interface["fragments"];
+    const abi = JSON.stringify(abiArray);
 
     return { storage, owner, otherAccount, abi };
   }
 
   describe("Deployment", function () {
+
+    const thorSoloUrl = "http://127.0.0.1:8669";
+    const config = {
+      chainId: 1,
+      from: undefined,
+      gas: 10000,
+      gasPrice: 10000,
+      gasMultiplier: 1,
+      url: thorSoloUrl,
+      accounts: 'remote',
+      timeout: 1000,
+      httpHeaders: { ['test']: 'test'} as { [name: string]: string },
+    } as unknown as HttpNetworkConfig;
+
     it("Default value is 0", async function () {
       const { storage, owner, otherAccount } = await deploy();
 
-      expect(await storage.retrieve()).to.equal(0);
+      const res = await storage.retrieve()
+
+      expect(res).to.equal(0);
     });
 
     it("Store/Retrieve is working", async function () {
@@ -36,9 +54,7 @@ describe("Storage", function () {
 
     it("Should emit StoreEvent", async function () {
       const { storage, owner, otherAccount } = await deploy();
-
-      await expect(storage.store(42))
-        .to.emit(storage, "StoreEvent");
+      await expect(storage.store(42)).to.emit(storage, "StoreEvent");
     });
 
     it("Should revert when > 100", async function () {
@@ -53,8 +69,20 @@ describe("Storage", function () {
 
     it("Should process tx with multiple clauses", async function () {
       const { storage, owner, otherAccount, abi } = await deploy();
+      
+      ethers.provider.getNetwork = async function() {
+        const chainId = await this._hardhatProvider.send("eth_chainId");
+        return new ethers.Network(this._networkName, chainId);
+      };
+      
       const network = await ethers.provider.getNetwork();
-      let builder = new ClausesBuilder(storage);
+
+      var wallet = createWallet(config);
+      wallet.import("0x99f0500549792796c14fed62011a51081dc5b5e68fe8bd8a13b86be829c4fd36");
+
+      var provider = await createProvider(config, wallet);
+
+      let builder = new ClausesBuilder(storage, provider);
       builder = await builder.withClause({
         args: [10],
         abi: abi,
